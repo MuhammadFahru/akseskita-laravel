35f8a5dd1c6c69b230f34032eb12905102a773ed
MuhammadFahru <fahrumuhammad.ozi@gmail.com>
2024-06-15 06:27:18 +0700

    feat: add Cors Middleware

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class Cors
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $response = $next($request);
+
+        $response->header('Access-Control-Allow-Origin', '*');
+        $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+        $response->header('Access-Control-Allow-Headers', 'Accept, authorization, Content-Type');
+        $response->header('Access-Control-Allow-Credentials', 'true');
+
+        return $response;
+    }
+}
