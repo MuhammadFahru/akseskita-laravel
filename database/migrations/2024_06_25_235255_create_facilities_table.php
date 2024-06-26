d197a7644c29179b52f9cdd01a2cf4d4d99125ef
MuhammadFahru <fahrumuhammad.ozi@gmail.com>
2024-06-26 07:01:28 +0700

    feat: add facilities migration and model

---
+++ database/migrations/2024_06_25_235255_create_facilities_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('facilities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('category');
+            $table->string('address');
+            $table->float('rating');
+            $table->boolean('is_favorite');
+            $table->string('latitude');
+            $table->string('longitude');
+            $table->string('blind_friendly_status')->nullable();
+            $table->string('disable_friendly_status')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('facilities');
+    }
+};
